3e6abbcaa50de853ccd9c858ef3d63b548ef4b3b
José Luis Pereira <pepelui@outlook.com>
2022-01-26 19:50:21 -0300

    Add scroll delay for models page

--- docs/.vuepress/configs/clientAppEnhance.js
+++ docs/.vuepress/configs/clientAppEnhance.js
@@ -1,15 +1,17 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
+  const modelPath = '/develop/Data/models.html'
+  const scrollBehavior = router.options.scrollBehavior
 
-
-
-    const scrollBehavior = router.options.scrollBehavior
-    router.options.scrollBehavior = async (...args) => {
-      await new Promise(resolve => setTimeout(resolve, 2000))
-      return scrollBehavior(...args)
-
+  router.options.scrollBehavior = async (to, from, saved) => {
+    if (
+      to.path != from.path && 
+      from.path !== modelPath &&
+      to.path === modelPath
+    ) {
+      await new Promise(resolve => setTimeout(resolve, 1000))
     }
-
-
+    return scrollBehavior(to, from, saved)
+  }
 })

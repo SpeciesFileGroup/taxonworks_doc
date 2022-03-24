2765b30fec307749062510fa20c6bd4b83f85000
José Luis Pereira <pepelui@outlook.com>
2022-03-24 13:21:11 -0300

    Support sort folders

--- docs/.vuepress/utils/generateSidebar.js
+++ docs/.vuepress/utils/generateSidebar.js
@@ -1,43 +1,39 @@
 const fs = require("fs")
 const path = require("path")
 const matter = require('gray-matter');
+const extension = [".md"]
 
-const getDirectories = source =>
-  fs.readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
+const getFilesAndDirectories = folderPath => fs
+  .readdirSync(folderPath, { withFileTypes: true })
+  .filter(item => item.isDirectory() || extension.includes(path.extname(item.name)))
 
 module.exports = function getSideBar(folder, text) {
-  const extension = [".md"]
   const folderPath = path.join(`${__dirname}/../../${folder}`)
-  const childrenFolders = getDirectories(folderPath)
-  const files = fs
-    .readdirSync(folderPath)
-    .filter(
-      item =>
-        fs.statSync(path.join(folderPath, item)).isFile() &&
-        extension.includes(path.extname(item))
-    ).map(file => `/${folder}/${file}`);
+  const filesAndDirectories = getFilesAndDirectories(folderPath)
 
-  const orderedFile = files.sort((a, b) => {
-    const fileA = fs.readFileSync(path.join(`${__dirname}/../../${a}`), 'utf8')
-    const fileB = fs.readFileSync(path.join(`${__dirname}/../../${b}`), 'utf8')
-    const objectA = matter(fileA)
-    const objectB = matter(fileB)
+  const getPosition = source => {
+    const sourcePath = source.isDirectory()
+      ? `${folderPath}/${source.name}/README.md`
+      : `${folderPath}/${source.name}`
 
-    const aPosition = objectA?.data?.sidebarPosition
-    const bPosition = objectB?.data?.sidebarPosition
+    if (fs.existsSync(sourcePath)) {
+      const file = fs.readFileSync(path.join(sourcePath), 'utf8')
+      const markdownObject = matter(file)
 
-    return aPosition - bPosition
-  })
+      return markdownObject?.data?.sidebarPosition
+    }
+  }
+
+  filesAndDirectories.sort((a, b) => getPosition(a) - getPosition(b))
 
   const sideBar = { 
     text,
     link: `/${folder}/`,
-    children: [].concat(
-      orderedFile, 
-      ...childrenFolders.map(folderName => getSideBar(`${folder}/${folderName}`, folderName))
-    )
+    children: [].concat(...filesAndDirectories.map(item => 
+      item.isDirectory() 
+        ? getSideBar(`${folder}/${item.name}`, item.name) 
+        : `/${folder}/${item.name}`
+    ))
   }
 
   return Array.isArray(sideBar) 

e669e9b339cb82b7ef7c350633c908afeadc5ba9
jlpereira <pepelui@outlook.com>
2023-04-29 01:53:48 -0300

    Set figcaption option

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 import { path } from '@vuepress/utils'
-import { defaultTheme } from'@vuepress/theme-default'
+import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 import { searchPlugin } from '@vuepress/plugin-search'
 import figurePlugin from './plugins/figure'
@@ -10,47 +10,49 @@ import * as pluginsLang from './configs/plugins'
 
 export default defineUserConfig({
   lang: 'en-US',
-  head: [
-    ['link', { rel: 'icon', href: '/images/favicon.svg' }]
-  ],
-  
+  head: [['link', { rel: 'icon', href: '/images/favicon.svg' }]],
+
   locales: {
     '/': {
       lang: 'en-US',
       title: 'TaxonWorks Docs',
-      description: 'TaxonWorks is a workbench with many tools for describing life. This site contains the documentation.',
+      description:
+        'TaxonWorks is a workbench with many tools for describing life. This site contains the documentation.',
       sidebar: sidebar.en
     },
     '/es/': {
       lang: 'es-ES',
       title: 'Documentación de TaxonWorks',
-      description: 'TaxonWorks es un banco de trabajo con muchas herramientas para describir la vida. Este sitio contiene la documentación',
-    },
-    
+      description:
+        'TaxonWorks es un banco de trabajo con muchas herramientas para describir la vida. Este sitio contiene la documentación'
+    }
   },
-  
+
   markdown: {
     code: {
       lineNumbers: false
     }
-  },    
-  
-  clientAppEnhanceFiles: path.resolve(__dirname, './configs/clientAppEnhance.js'),
-  
-  extendsMarkdown: md => {
-    md.use(taskListPlugin)
-    md.use(figurePlugin)
   },
-  
+
+  clientAppEnhanceFiles: path.resolve(
+    __dirname,
+    './configs/clientAppEnhance.js'
+  ),
+
+  extendsMarkdown: (md) => {
+    md.use(taskListPlugin)
+    md.use(figurePlugin, { figcaption: true })
+  },
+
   theme: defaultTheme({
     docsRepo: 'https://github.com/SpeciesFileGroup/taxonworks_doc',
     docsBranch: 'development',
     docsDir: 'docs',
     editLinkPattern: ':repo/edit/:branch/:path',
-    
+
     logo: '/images/taxonworks-black-circle.svg',
     logoDark: '/images/taxonworks-white-circle.svg',
-    
+
     locales: {
       '/': {
         lang: 'en-US',
@@ -62,30 +64,30 @@ export default defineUserConfig({
         selectLanguageName: 'Español',
         selectLanguageText: 'Español',
         selectLanguageAriaLabel: 'Español',
-        
+
         sidebar: sidebar.es,
-        
+
         editLinkText: 'Edita esta pagina en GitHub',
         lastUpdatedText: 'Ultima actualización',
         contributorsText: 'Contribuidores',
-        
+
         tip: 'Tip',
         warning: 'Advertencia',
         danger: 'Cuidado',
-        
+
         notFound: [
           'Esta pagina no existe',
-          'Parece que ha entrado a un link erroneo',
+          'Parece que ha entrado a un link erroneo'
         ],
         backToHome: 'Volver al inicio',
-        
+
         openInNewWindow: 'Abrir en nueva ventana',
         toggleDarkMode: 'Alternar al modo oscuro',
-        toggleSidebar: 'Alternar barra lateral',
-      },
+        toggleSidebar: 'Alternar barra lateral'
+      }
     }
   }),
-  
+
   plugins: [
     searchPlugin({
       locales: {
